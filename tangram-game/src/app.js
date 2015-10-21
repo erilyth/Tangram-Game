34ac1edccb633462f5f4a5d927bcf2f94249e718
erilyth <vishal.batchu@students.iiit.ac.in>
2015-10-21 14:02:28 +0530

    Added multiple board mode

--- tangram-game/src/app.js
+++ tangram-game/src/app.js
@@ -1,6 +1,5 @@
 //BlockWidth is 20 and in other places its directly coded as 20.
 //Board currently has 15 rows and 15 columns where the victory area is a 4x3 rectangle.
-//There are 2 pieces currently.
 
 var Piece= cc.Class.extend({
 	color:"blue",
@@ -107,11 +106,12 @@ var Board = cc.Class.extend({
 	spriteBlocks:[],
 	belongPieces:[],
 	countArray:[],
-	ctor:function(){
-		this.initpositionarr();
+	victoryArray:[],
+	ctor:function(victoryposition){
+		this.initpositionarr(victoryposition);
 		// Custom initialization
 	},
-	initpositionarr:function(){
+	initpositionarr:function(victoryposition){
 		this.positionarr=new Array(15);
 		this.spriteBlocks=new Array(15);
 		this.belongPieces=new Array(15);
@@ -119,12 +119,15 @@ var Board = cc.Class.extend({
 		for(i=0;i<15;i++){
 			this.spriteBlocks[i]= new Array(15);
 			this.positionarr[i]=new Array(15);
+			this.victoryArray[i]=new Array(15);
 			for(j=0;j<15;j++){
-				if(i>=4 && i<8 && j>=4 && j<7){
+				if(victoryposition[i][j]==1){
+					this.victoryArray[i][j]=1;
 					this.spriteBlocks[i][j]= new cc.Sprite.create(res.PlaceBoard_png);
 				}
 				else{
-					this.spriteBlocks[i][j]=  new cc.Sprite.create(res.Board_png);
+					this.victoryArray[i][j]=0;
+					this.spriteBlocks[i][j]= new cc.Sprite.create(res.Board_png);
 				}
 				this.positionarr[i][j]=-1;
 				this.spriteBlocks[i][j].setPosition(new cc.p(20*i,20*j));
@@ -137,12 +140,14 @@ var Board = cc.Class.extend({
 		for(i=0;i<15;i++){
 			this.countArray[i]=0;
 		}
-		for(i=4;i<8;i++){
-			for(j=4;j<7;j++){
-				if(this.positionarr[i][j]==-1){
-					return 0;
+		for(i=0;i<15;i++){
+			for(j=0;j<15;j++){
+				if(this.victoryArray[i][j]==1){
+					if(this.positionarr[i][j]==-1){
+						return 0;
+					}
+					this.countArray[this.positionarr[i][j]]+=1;
 				}
-				this.countArray[this.positionarr[i][j]]+=1;
 			}
 		}
 		for(i=0;i<15;i++){
@@ -171,7 +176,19 @@ var GameMode1Layer = cc.Layer.extend({
         
         
         //(0,0) is the bottom left point.
-        var board = new Board();
+        var boardarray=new Array(15);
+        for(i=0;i<15;i++){
+        	boardarray[i]=new Array(15);
+        	for(j=0;j<15;j++){
+        		boardarray[i][j]=0;
+        	}
+        }
+        for(i=4;i<8;i++){
+        	for(j=4;j<7;j++){
+        		boardarray[i][j]=1;
+        	}
+        }
+        var board = new Board(boardarray);
         //var board = new BoardSprite(res.CloseSelected_png,cc.rect(300,300,200,200));
         var pieceList = new Array(5);
         var noOfPieces=2;
@@ -314,7 +331,19 @@ var GameMode2Layer = cc.Layer.extend({
 
 
 		//(0,0) is the bottom left point.
-		var board = new Board();
+		var boardarray=new Array(15);
+		for(i=0;i<15;i++){
+			boardarray[i]=new Array(15);
+			for(j=0;j<15;j++){
+				boardarray[i][j]=0;
+			}
+		}
+		for(i=4;i<8;i++){
+			for(j=4;j<7;j++){
+				boardarray[i][j]=1;
+			}
+		}
+		var board = new Board(boardarray);
 		//var board = new BoardSprite(res.CloseSelected_png,cc.rect(300,300,200,200));
 		var pieceList = new Array(5);
 		var noOfPieces=3;
@@ -450,6 +479,186 @@ var GameMode2Layer = cc.Layer.extend({
 	}
 });
 
+var GameMode3Layer = cc.Layer.extend({
+	sprite:null,
+	ctor:function () {
+		//////////////////////////////
+		// 1. super init first
+		this._super();
+
+		/////////////////////////////
+		// 2. add a menu item with "X" image, which is clicked to quit the program
+		//    you may modify it.
+		// ask the window size
+
+
+		//(0,0) is the bottom left point.
+		var boardarray=new Array(15);
+		for(i=0;i<15;i++){
+			boardarray[i]=new Array(15);
+			for(j=0;j<15;j++){
+				boardarray[i][j]=0;
+			}
+		}
+		for(i=4;i<8;i++){
+			for(j=4;j<7;j++){
+				boardarray[i][j]=1;
+			}
+		}
+		for(i=9;i<13;i++){
+			for(j=9;j<12;j++){
+				boardarray[i][j]=1;
+			}
+		}
+		var board = new Board(boardarray);
+		//var board = new BoardSprite(res.CloseSelected_png,cc.rect(300,300,200,200));
+		var pieceList = new Array(5);
+		var noOfPieces=4;
+		var piece = new Piece(1,0,0);
+		var piece2 = new Piece(10,5,1);
+		var piece3 = new Piece(12,1,2);
+		var piece4 = new Piece(2,10,3);
+		board.belongPieces[0]=piece;
+		board.belongPieces[1]=piece2;
+		board.belongPieces[2]=piece3;
+		board.belongPieces[3]=piece4;
+		//var piece3 = new Piece(7,1,2);
+		var arr=new Array(3);
+		var arr2=new Array(3);
+		var arr3=new Array(3);
+		var arr4=new Array(3);
+		for(i=0;i<3;i++){
+			arr[i]=new Array(3);
+			arr2[i]=new Array(3);
+			arr3[i]=new Array(3);
+			arr4[i]=new Array(3);
+			for(j=0;j<3;j++){
+				arr2[i][j]=1;
+			}
+		}
+		arr[0][0]=1;
+		arr[0][1]=1;
+		arr[0][2]=1;
+		arr3[0][0]=1;
+		arr3[0][1]=1;
+		arr3[0][2]=1;
+		arr3[1][0]=1;
+		arr3[2][0]=1;
+		arr4[1][1]=1;
+		arr4[1][2]=1;
+		arr4[2][1]=1;
+		arr4[2][2]=1;
+		arr4[0][1]=1;
+		arr4[0][2]=1;
+		arr4[2][0]=1;
+		piece.initpositionarr(arr);
+		piece2.initpositionarr(arr2);
+		piece3.initpositionarr(arr3);
+		piece4.initpositionarr(arr4);
+		pieceList[0]=piece;
+		pieceList[1]=piece2;
+		pieceList[2]=piece3;
+		pieceList[3]=piece4;
+		for(i=0;i<15;i++){
+			for(j=0;j<15;j++){
+				this.addChild(board.spriteBlocks[i][j]);
+			}
+		}
+		for(i=0;i<3;i++){
+			for(j=0;j<3;j++){
+				for(k=0;k<noOfPieces;k++){
+					if(pieceList[k].positionarr[i][j]==1){
+						this.addChild(pieceList[k].spriteBlocks[i][j]);
+					}
+				}
+			}
+		}
+		for(k=0;k<noOfPieces;k++){
+			pieceList[k].placePiece(pieceList[k].basePositionX,pieceList[k].basePositionY,board);
+		}
+
+		var clickOffsetXBlock=0;
+		var clickOffsetYBlock=0;
+		var originalBaseX=0;
+		var originalBaseY=0;
+		var pieceSelected=-1;
+
+		if (cc.sys.capabilities.hasOwnProperty('mouse')){ //Set up mouse events
+			cc.eventManager.addListener(
+					{
+						event: cc.EventListener.MOUSE,
+						onMouseDown:function(event){
+							if (event.getButton() == cc.EventMouse.BUTTON_LEFT){
+								for(k=0;k<noOfPieces;k++){
+									//cc.log(event.getLocationX());
+									if(pieceSelected==-1){
+										clickOffsetXBlock=0;
+										clickOffsetYBlock=0;
+									}
+									var click=cc.p(event.getLocationX(),event.getLocationY());
+									var x=Math.floor((event.getLocationX())/20);
+									var y=Math.floor((event.getLocationY())/20);
+									//cc.log(x + " " + y + " Clicked at")
+									for(i=0;i<3;i++){
+										for(j=0;j<3;j++){
+											if(pieceList[k].positionarr[i][j]==1 && cc.rectContainsPoint(new cc.Rect((pieceList[k].basePositionX+i)*20,(pieceList[k].basePositionY+j)*20,20,20),click)){
+												originalBaseX=pieceList[k].basePositionX;
+												originalBaseY=pieceList[k].basePositionY;
+												pieceSelected=k;
+												clickOffsetXBlock=i;
+												clickOffsetYBlock=j;
+											}
+										}
+									}
+									//cc.log("Left mouse clicked at "+event.getLocationX());
+								}
+							}
+						},
+						onMouseMove: function (event) {         
+							//Move the position of current button sprite
+							if(pieceSelected!=-1){
+								for(i=0;i<3;i++){
+									for(j=0;j<3;j++){
+										if(pieceList[pieceSelected].positionarr[i][j]==1){
+											var target=pieceList[pieceSelected].spriteBlocks[i][j].getPosition();
+											var delta = event.getDelta();
+											target.x += delta.x;
+											target.y += delta.y;
+											pieceList[pieceSelected].spriteBlocks[i][j].setPosition(target);
+										}
+									}
+								}
+							}
+						},
+						onMouseUp:function(event){
+							if (event.getButton() == cc.EventMouse.BUTTON_LEFT){
+								if(pieceSelected!=-1){
+									var click2=cc.p(event.getLocationX(),event.getLocationY());
+									var x=Math.floor((event.getLocationX())/20);
+									var y=Math.floor((event.getLocationY())/20);
+									cc.log((x-clickOffsetXBlock) + " " + (y-clickOffsetYBlock) + " Left at")
+									if(pieceList[pieceSelected].checkPiece(x-clickOffsetXBlock, y-clickOffsetYBlock, board)==1){
+										pieceList[pieceSelected].placePiece(x-clickOffsetXBlock,y-clickOffsetYBlock,board);
+									}
+									else{
+										pieceList[pieceSelected].placePiece(originalBaseX,originalBaseY,board);
+									}
+									pieceSelected=-1;
+								}
+								if(board.checkVictory()==1){
+									cc.log("YOU WIN!");
+								}
+								//cc.log("Left mouse released at "+event.getLocationX());
+							}
+						}
+
+					}, this);
+		}
+
+		return true;
+	}
+});
+
 var MenuLayer = cc.Layer.extend({
 	ctor:function () {
 		//////////////////////////////
@@ -461,16 +670,14 @@ var MenuLayer = cc.Layer.extend({
 		sprite.setPosition(cc.p(size.width/2,size.height/2));
 		sprite.setScaleX(0.5);
 		sprite.setScaleY(0.2);
-		var menuItem1 = new cc.MenuItemFont("Play Mode 1",startGameMode1);
+		var menuItem1 = new cc.MenuItemFont("Play Mode 1", startGameMode1);
 		var menuItem2 = new cc.MenuItemFont("Play Mode 2", startGameMode2);
-		var menuItem3 = new cc.MenuItemFont("Quit", quitGame);
-		var menu = new cc.Menu(menuItem1,menuItem2,menuItem3);
+		var menuItem3 = new cc.MenuItemFont("Play Mode 3", startGameMode3);
+		var menuItem4 = new cc.MenuItemFont("Quit", quitGame);
+		var menu = new cc.Menu(menuItem1,menuItem2,menuItem3,menuItem4);
 		menu.alignItemsVerticallyWithPadding(50);
 		this.addChild(menu);
 		return true;
-	},
-	buttoner:function(){
-		return this.buttonclicked;
 	}
 });
 
@@ -484,6 +691,10 @@ var startGameMode1=function(){
 
 var startGameMode2=function(){
 	cc.director.runScene(new GameMode2Scene());
+}
+
+var startGameMode3=function(){
+	cc.director.runScene(new GameMode3Scene());
 }
 
 var MenuScene = cc.Scene.extend({
@@ -508,5 +719,13 @@ var GameMode2Scene = cc.Scene.extend({
 		this._super();
 		var layer3=new GameMode2Layer();
 		this.addChild(layer3);
+	}
+});
+
+var GameMode3Scene = cc.Scene.extend({
+	onEnter:function(){
+		this._super();
+		var layer4=new GameMode3Layer();
+		this.addChild(layer4);
 	}
 });
