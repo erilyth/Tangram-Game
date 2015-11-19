a1fc696f3d9eb30da3551d4321cc59dd9e8c32e3
Syamantak Das <gogollian@gmail.com>
2015-11-19 17:33:07 +0800

    changed some resources for block pictures

--- tangram-game/src/resource.js
+++ tangram-game/src/resource.js
@@ -5,10 +5,10 @@ var res = {
     CloseNormal_png : "res/CloseNormal.png",
     CloseSelected_png : "res/CloseSelected.png",
     Youwin_png : "res/You-Win.png",	
-    Piece1_png: "res/piece1.png",
+    Piece1_png: "res/simpleblock4.png",
     Piece2_png: "res/piece2.png",
-    Piece3_png: "res/piece3.png",
-    Piece4_png: "res/piece4.png",
+    Piece3_png: "res/simpleblock2.png",
+    Piece4_png: "res/simpleblock3.png",
     Background_png: "res/background.png",
     GameBackground_png: "res/gamebackground.png"
 };
